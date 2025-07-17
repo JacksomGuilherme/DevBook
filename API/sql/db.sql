a714b9b999a40835e86826ae9e597b9d15139d57
JacksomGuilherme <jacksom.santos@autbank.com.br>
2025-07-17 16:49:40 -0300

    Commit inicio do desenvolvimento das rotas e serviços referentes as publicações

--- API/sql/db.sql
+++ API/sql/db.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 use devbook;
 
-DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS seguidores;
+DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
     id int auto_increment primary key,
@@ -23,4 +24,18 @@ CREATE TABLE seguidores(
     ON DELETE CASCADE,
 
     PRIMARY KEY(usuario_id, seguidor_id)
+);
+
+CREATE TABLE publicacoes(
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+    
+    autor_id int not null,
+    FOREIGN KEY (autor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    curtidas int default 0
+    criadaEm timestamp default current_timestamp
 );
---
+++ API/src/repositorios/publicacoes.go
@@ -0,0 +1,76 @@
+package repositorios
+
+import (
+	"api/src/modelos"
+	"database/sql"
+)
+
+type Publicacoes struct {
+	db *sql.DB
+}
+
+// NovoRepositorioDePublicacoes cria um repositório de publicações
+func NovoRepositorioDePublicacoes(db *sql.DB) *Publicacoes {
+	return &Publicacoes{db}
+}
+
+func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, error) {
+	statement, erro := repositorio.db.Prepare("INSERT INTO PUBLICACOES (TITULO, CONTEUDO, AUTOR_ID) VALUES (?, ?, ?)")
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacao.AutorID)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
+
+}
+
+func (repositorio Publicacoes) BuscarPublicacoes() {
+
+}
+
+func (repositorio Publicacoes) BuscarPorId(ID uint64) (modelos.Publicacao, error) {
+
+	linhas, erro := repositorio.db.Query(
+		"SELECT ID, TITULO, CONTEUDO, AUTOR_ID, CURTIDAS, CRIADAEM FROM PUBLICACOES WHERE ID = ?",
+		ID,
+	)
+	if erro != nil {
+		return modelos.Publicacao{}, erro
+	}
+	defer linhas.Close()
+
+	var publicacao modelos.Publicacao
+	if linhas.Next() {
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+		); erro != nil {
+			return modelos.Publicacao{}, erro
+		}
+	}
+
+	return publicacao, nil
+}
+
+func (repositorio Publicacoes) Atualizar() {
+
+}
+
+func (repositorio Publicacoes) Deletar() {
+
+}
