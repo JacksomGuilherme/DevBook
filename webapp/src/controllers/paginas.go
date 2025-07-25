abfc3ad5eb54b3ea70678e1dfc969ae30b590b08
JacksomGuilherme <jacksom.santos@autbank.com.br>
2025-07-25 08:51:21 -0300

    Commit da página home e criação do pacote para consumo de serviços que exigem autenticação via token

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,7 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/modelos"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -13,4 +19,28 @@ func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 // CarregarPaginaDeCadastroDeUsuario vai renderizar a tela de cadastro de usuários
 func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro", nil)
+}
+
+// CarregarPaginaPrincipal vai renderizar a pagina principal com as publicações
+func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
+	url := fmt.Sprintf("%s/publicacoes", config.ApiUrl)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.Erro{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodDeErro(w, response)
+		return
+	}
+
+	var publicacoes []modelos.Publicacao
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.Erro{Erro: erro.Error()})
+		return
+	}
+
+	utils.ExecutarTemplate(w, "home", nil)
 }
